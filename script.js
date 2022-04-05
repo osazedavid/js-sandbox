d3866f04029d179acc9a57a57a91478338419e2c
osazedavid <osazeoto@yahoo.com>
2022-04-05 20:15:00 +0100

    Js function

---
+++ script.js
@@ -0,0 +1,43 @@
+// var operator = prompt("enter operator");
+
+
+// var firstNumber = prompt("enter first number");
+// var secondNumber = prompt("enter second number");
+
+// if(operator === "add") {
+//     alert(Number(firstNumber) + Number(secondNumber))
+// }
+// else if(operator === "subtract") {
+//     alert(Number(firstNumber) - Number(secondNumber))
+// }
+// else if(operator === "multiply") {
+//     alert(Number(firstNumber) * Number(secondNumber))
+// }
+// else if(operator === "divide") {
+//     alert(Number(firstNumber) / Number(secondNumber))
+// }
+// else {
+//     alert("operator invalid")
+// }
+
+function add(firstNumber, secondNumber) {
+    return (firstNumber + secondNumber);
+    console.log(add);
+}
+
+function subtract(firstNumber, secondNumber) {
+    return (firstNumber - secondNumber);
+    console.log(subtract);
+}
+
+function multiply(firstNumber, secondNumber) {
+    return (firstNumber * secondNumber);
+    console.log(multiply);
+}
+
+function divide(firstNumber, secondNumber) {
+    return (firstNumber / secondNumber);
+    console.log(divide);
+}
+
+add(8, 7);
