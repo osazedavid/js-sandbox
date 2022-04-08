6080f750993f7d055f7fb8a25537f6949d683a1b
osazedavid <osazeoto@yahoo.com>
2022-04-08 20:09:15 +0100

    Create object.js

---
+++ object.js
@@ -0,0 +1,28 @@
+
+var user1 = {
+    username: "david1",
+    password: "osaze123" 
+};
+
+var database = [
+    {
+        username: "david1",
+        password: "osaze123"
+
+    }
+];
+
+var newsfeed = [
+    {
+        username: "",
+        timeline: ""
+    },
+    {
+        username: "",
+        timeline: ""
+    },
+    {
+        username: "",
+        timeline: ""
+    }
+];
